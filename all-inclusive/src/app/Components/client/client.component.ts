01d88637a9e6d38cf61bb8ae5905e4a5a488c60c
Elyes-Ferjani <officialef@hotmail.com>
2020-11-03 20:48:06 +0100

    Inputs Of Props In Client And Company Of Type Any(Object)

--- all-inclusive/src/app/Components/client/client.component.ts
+++ all-inclusive/src/app/Components/client/client.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-client',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientComponent implements OnInit {
   // user comming as a props from signin and to be sent as a props to messages component
-  user: any = {}
+  @Input() user: any ;
   eventsArray: any = []
   constructor() { }
 
--- all-inclusive/src/app/Components/company/company.component.ts
+++ all-inclusive/src/app/Components/company/company.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-company',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./company.component.css']
 })
 export class CompanyComponent implements OnInit {
-  company: any = {}
+  @Input() company: any ;
   constructor() { }
 
   ngOnInit(): void {
