b57ff977f22603dafc05eca580e0253725905627
Elyes-Ferjani <officialef@hotmail.com>
2020-11-03 20:20:21 +0100

    Company Componenet Is Ready

--- all-inclusive/src/app/Components/company/company.component.ts
+++ all-inclusive/src/app/Components/company/company.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./company.component.css']
 })
 export class CompanyComponent implements OnInit {
-
+  company: any = {}
   constructor() { }
 
   ngOnInit(): void {
