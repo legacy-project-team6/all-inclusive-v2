c46db8cfb87204bdc4e95e52ca4f31ee12f8c8f4
Elyes-Ferjani <officialef@hotmail.com>
2020-11-05 22:02:16 +0100

    Client And Company Http Requests Server

--- all-inclusive/src/app/user.service.ts
+++ all-inclusive/src/app/user.service.ts
@@ -29,4 +29,37 @@ export class UserService {
   public addNewCompany(options){
     return this.httpClient.post(this._urlCompany + '/add', options)
   }
+
+  // log in client 
+
+  public logInClient(options){
+    return this.httpClient.post(this._urlClient + '/login', options)
+  }
+
+  // log in company 
+
+  public logInCompany(options){
+    return this.httpClient.post(this._urlCompany + '/login', options)
+  }
+
+  // delete client / company
+
+  public deleteAllClient(options){
+    return this.httpClient.delete(this._urlClient, options)
+  }
+
+  public deleteAllCompany(options){
+    return this.httpClient.delete(this._urlCompany, options)
+  }
+
+  // delete client by id/ company by id
+
+  public deleteClientById(options){
+    return this.httpClient.delete(this._urlClient + '/:id', options)
+  }
+
+  public deleteCompanyById(options){
+    return this.httpClient.delete(this._urlCompany + '/:id', options)
+  }
+
 }
