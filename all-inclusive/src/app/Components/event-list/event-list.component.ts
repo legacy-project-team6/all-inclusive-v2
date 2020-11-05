593d89a34ef46c9ef2b55247bc6eff55b7f03be8
HaykelHadjSalem <haykel_hadj@yahoo.fr>
2020-11-05 12:32:00 +0100

    resolving conflicts

--- all-inclusive/src/app/Components/event-list/event-list.component.ts
+++ all-inclusive/src/app/Components/event-list/event-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FavoritService } from 'src/app/favorit.service';
 
 @Component({
   selector: 'app-event-list',
@@ -6,57 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-list.component.css']
 })
 export class EventListComponent implements OnInit {
-  events=[
-    {
-      name:"hiking event",
-      type:"hiking",
-      place:"Nabeul",
-      date:"25/12/2020",
-      description:"welcome to our hiking",
-      imgUrl:"https://cdn.siasat.com/wp-content/uploads/2019/05/events.png",
-      like:0,
-      disLike:0
-    },
-    {
-      name:"hiking event",
-      type:"hiking",
-      place:"Nabeul",
-      date:"25/12/2020",
-      description:"welcome to our hiking",
-      imgUrl:"https://www.day2trust.com/wp-content/uploads/2016/06/events.jpg",
-      like:0,
-      disLike:0
-    },
-    {
-      name:"hiking event",
-      type:"hiking",
-      place:"Nabeul",
-      date:"25/12/2020",
-      description:"welcome to our hiking",
-      imgUrl:"https://lh3.googleusercontent.com/proxy/WxypDW-QxOQ_Cv2-_2fT-tIcvo7Dh9rpc-gZkhhoFD5PF63Zu5YOFcVFbIACm0uPA2_cnAVehFQNN8GR-HvnX9Iv-nE7yzziQyQzXX0OwweXRcaLf7qBFjAQcQlO152Z-zOm-VY9qgjx",
-      like:0,
-      disLike:0
-    },{
-      name:"hiking event",
-      type:"hiking",
-      place:"Nabeul",
-      date:"25/12/2020",
-      description:"welcome to our hiking",
-      imgUrl:"https://cdn-az.allevents.in/events1/banners/4bb937881e2188b08717c5f5c5621ae9641ecc4677721ad4a9473d81a95d1634-rimg-w960-h502-gmir.jpg?v=1584682243",
-      like:0,
-      disLike:0
-    }
-  ]
-  constructor() { }
+  
+  events=this.favoritService.events;
+  listCondition=this.favoritService.listCondition;
+  constructor(
+    private favoritService: FavoritService
+  ) { }
 
   ngOnInit(): void {
   }
   addLike(eventId){
-    this.events[eventId].like=this.events[eventId].like+1;
+    this.favoritService.addLike(eventId);
   }
 
   addDisLike(eventId){
-    this.events[eventId].disLike=this.events[eventId].disLike+1;
+    this.favoritService.addDisLike(eventId);
   }
 
+  participate(event){
+    this.favoritService.participate(event);
+  }
 }
--- all-inclusive/src/app/app-routing.module.ts
+++ all-inclusive/src/app/app-routing.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './Components/navbar/navbar.component';
 import { LoginComponent } from './Components/login/login.component';
 import { SignupComponent } from './Components/signup/signup.component';
-
+import { FooterComponent } from './Components/footer/footer.component';
+import { SlideShowComponent } from './Components/slide-show/slide-show.component';
+import { EventListComponent } from './Components/event-list/event-list.component';
+import { ClientComponent } from './Components/client/client.component';
+import { CompanyComponent } from './Components/company/company.component';
+import { MessagesComponent } from './Components/messages/messages.component';
+import { PurchaseComponent } from './Components/purchase/purchase.component';
+import { HomeComponent } from './Components/home/home.component';
 const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' },
-  {path: 'login', component: LoginComponent},
-  {path: 'signup', component: SignupComponent}
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {path: 'slideshow', component: SlideShowComponent },
+  {path: 'footer', component: FooterComponent },
+  {path: 'signup', component: SignupComponent },
+  {path: 'login', component: LoginComponent },
+  {path: 'navbar', component: NavbarComponent },
+  {path: 'eventlist', component: EventListComponent },
+  {path: 'client', component: ClientComponent },
+  {path: 'company', component: CompanyComponent },
+  {path: 'message', component: MessagesComponent },
+  {path: 'purchase', component: PurchaseComponent },
+  {path: 'home', component: HomeComponent }
+
 ];
 
 @NgModule({
@@ -14,3 +32,16 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [
+  SlideShowComponent,
+  FooterComponent,
+  SignupComponent,
+  LoginComponent,
+  NavbarComponent,
+  EventListComponent,
+  ClientComponent,
+  CompanyComponent,
+  MessagesComponent,
+  PurchaseComponent,
+  HomeComponent
+  ]
